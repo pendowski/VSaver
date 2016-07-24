e9b29ce05af00ef43427d7f0d8c5e12286958b25
Jaroslaw Pendowski <jarek@macoscope.net>
2016-07-24 15:47:24 +0200

    WIP

--- VSaver/YouTubeProvider.swift
+++ VSaver/YouTubeProvider.swift
@@ -10,6 +10,7 @@ import Foundation
 
 
 final class YouTubeProvider: Provider {
+    private let compatibleItags = ["95", "299", "266", "137", "22", "136", "135", "134", "18", "133", "160"]
 
     func isValidURL(url: NSURL) -> Bool {
         guard let host = url.host else {
@@ -18,51 +19,64 @@ final class YouTubeProvider: Provider {
 
         return host.containsString("youtube.com") || host.containsString("youtu.be")
     }
-
+    
     func getVideoURL(url: NSURL, completion: (url: NSURL?) -> Void) {
         let failed = { completion(url: nil) }
-
+        
         guard let id = self.idFromURL(url), videoInfoURL = NSURL(string: "http://www.youtube.com/get_video_info?video_id=\(id)")  else {
             return failed()
         }
-
+        
         let task = NSURLSession.sharedSession().dataTaskWithURL(videoInfoURL) { (data, response, error) in
-
+            
             guard let data = data, string = String(data: data, encoding: NSUTF8StringEncoding)?.stringByRemovingPercentEncoding else {
                 return failed()
             }
-
+            
             var urls: [String : String] = [ : ]
-
-            var quality: String?
+            
+            var itag: String?
             var url: String?
-
-            string.componentsSeparatedByString("&").forEach({ (item) in
-                let b = item.componentsSeparatedByString("=")
-
-                if b[0] == "quality" {
-                    quality = b[1]
+            
+            string.componentsSeparatedByString("&").forEach({ line in
+                let components = line.componentsSeparatedByString("=")
+                
+                let key = components[0]
+                if key != "itag" && key != "url" {
+                    return
                 }
-
-                if b[0] == "url" {
-                    url = b[1]
+                
+                guard let value = components[1].stringByRemovingPercentEncoding else {
+                    return
                 }
-
-                if let q = quality, u = url?.stringByRemovingPercentEncoding {
-                    urls[q] = u
-
-                    quality = nil
+                
+                if key == "itag" {
+                    itag = value
+                }
+                if key == "url" {
+                    url = value
+                }
+                
+                if let tag = itag, videoUrl = url {
+                    urls[tag] = videoUrl
+                    
+                    itag = nil
                     url = nil
                 }
+                
             })
-
-            guard let videoUrl = urls["hd1080"] ?? urls["hd720"] ?? urls.first?.1 else {
+            
+            guard let videoUrl = self.compatibleItags.flatMap({
+                return urls[$0]
+            }).first else {
                 return failed()
             }
-
-            completion(url: NSURL(string: videoUrl))
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                completion(url: NSURL(string: videoUrl))
+            })
         }
-
+        
         task.resume()
     }
 
---
+++ VSaverTester/VideoPlayer.swift
@@ -0,0 +1,96 @@
+//
+//  VideoPlayer.swift
+//  VSaver
+//
+//  Created by Jarosław Pendowski on 24/07/16.
+//  Copyright © 2016 Jaroslaw Pendowski. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+enum PlayerMode {
+    case Loop
+    case Random
+    case Sequence
+}
+
+final class VideoPlayer {
+    private let player: AVPlayer
+    private let providers: [Provider]
+    private var urls: [NSURL] = []
+    private var urlIndex: Int = -1
+    
+    var mode: PlayerMode = .Random
+    
+    init(player: AVPlayer) {
+        self.player = player
+        self.providers = [ YouTubeProvider() ]
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.videoDidEnd(_:)), name: AVPlayerItemDidPlayToEndTimeNotification, object: nil)
+        
+    }
+    
+    func setQueue(urls: [NSURL]) {
+        self.player.pause()
+        
+        self.urlIndex = -1
+        
+        self.urls = urls
+        
+        self.playNext()
+    }
+    
+    private func playNext() {
+        guard urls.count > 0 else {
+            return
+        }
+        
+        var index: Int = self.urlIndex
+        let total = self.urls.count
+        
+        switch mode {
+        case .Loop:
+            if index < 0 {
+                index = random() % total
+            }
+        case .Random:
+            index = random() % total
+        case .Sequence:
+            index = (index + 1) % total
+        }
+        
+        let url = self.urls[index]
+        guard let provider = self.providers.filter({ $0.isValidURL(url) }).first else { // url is invalid, try another one
+            self.urlIndex = -1
+            self.urls.removeAtIndex(index)
+            self.playNext()
+            return
+        }
+        
+        self.urlIndex = index
+        
+        provider.getVideoURL(url) { videoUrl in
+            
+            guard let videoUrl = videoUrl else {
+                return
+            }
+            
+            print("Playing: ", videoUrl)
+            let playerItem = AVPlayerItem(URL: videoUrl)
+            self.player.replaceCurrentItemWithPlayerItem(playerItem)
+            self.player.volume = 0
+            self.player.actionAtItemEnd = .None
+            
+            dispatch_async(dispatch_get_main_queue(), { 
+                self.player.play()
+            })
+        }
+    }
+    
+    @objc func videoDidEnd(notification: NSNotification) {
+        if let item = notification.object as? AVPlayerItem where self.player.currentItem == item {
+            self.playNext()
+        }
+    }
+}
