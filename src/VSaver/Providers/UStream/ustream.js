a1fa96aa983f5ef276d6c72d58d0374e9a396208
Jarek Pendowski <jarek@pendowski.com>
2019-03-16 18:53:32 +0100

    Updated script for Ustream

--- src/VSaver/Providers/UStream/ustream.js
+++ src/VSaver/Providers/UStream/ustream.js
@@ -1,17 +1,22 @@
 function vsaverMain() {
-    document.querySelector('.playScreen').click()
+    const playButton = document.querySelector('.playScreen') || document.querySelector('#play')
+    if (playButton) {
+        playButton.click()
+    }
     
     let interval = null;
     let findVideo = () => {
         let video = document.querySelector('video');
         if (video && video.src && video.src.indexOf('http') === 0) {
             let title = window.title
-            let titleElement = document.querySelector('.playScreen .title')
+            let titleElement = document.querySelector('.title')
             if (titleElement && titleElement.innerText) {
                 title = titleElement.innerText
             }
-            vsaverCompletion(video.src, title)
-            clearInterval(interval)
+            if (vsaverCompletion) {
+                vsaverCompletion(video.src, title)
+                clearInterval(interval)
+            }
         }
     }
     interval = setInterval(findVideo, 500)
